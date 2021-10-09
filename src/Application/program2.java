68c1f8164ca44976cdd24bd26bda6e609391ac11
Jorge Garcia <jsgj10@gmail.com>
2021-10-09 16:57:40 -0300

    Adjusted project

--- src/Application/program2.java
+++ src/Application/program2.java
@@ -1,11 +1,10 @@
 package Application;
 
-public class program {
+public class program2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- int test;
- double test2;
+
 	}
 
 }
